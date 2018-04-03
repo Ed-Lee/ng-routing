69da1a1590a258d1cb324811cf3da9721b95358a
Ed Lee <ronder.lee@gmail.com>
2018-04-03 10:08:27 +0800

    Route Parameters 1. Configuring a Parameterized Route (path: 'products/:id') 2. Populate Route Parameters ([routerLink]="['/products', product.id]" 3. Reading Route Parameters: Snapshot (注入ActivatedRoute, route.snapshot.params['id'] 但對於相同路經解析時，為了效能組件不會重新建立，所以將此法寫在ngOnInit()會不被更新

--- src/app/products/product-edit.component.ts
+++ src/app/products/product-edit.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MessageService } from '../messages/message.service';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     templateUrl: './product-edit.component.html',
     styleUrls: ['./product-edit.component.css']
 })
-export class ProductEditComponent {
+export class ProductEditComponent implements OnInit{
     pageTitle: string = 'Product Edit';
     errorMessage: string;
 
     product: IProduct;
 
     constructor(private productService: ProductService,
-                private messageService: MessageService) { }
+                private messageService: MessageService,
+                private route: ActivatedRoute) { }
+
+    ngOnInit() {
+      this.getProduct(+this.route.snapshot.params['id']);
+    }
 
     getProduct(id: number): void {
         this.productService.getProduct(id)
