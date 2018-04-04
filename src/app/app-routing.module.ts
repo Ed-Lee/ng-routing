9e27a550272d80b56f97f6b607f6d3e107adc583
Ed Lee <ronder.lee@gmail.com>
2018-04-04 17:12:04 +0800

    Lazy Loading 4. Custom Preloading Strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {LoginComponent} from "./user/login.component";
 import {ProductListComponent} from "./products/product-list.component";
 import {PageNotFoundComponent} from "./page-not-found.component";
 import { AuthGuardService } from './user/auth-guard.service';
+import { SelectiveStrategyService } from './selective-strategy.service';
 
 const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
   {path: 'products',
-    canLoad: [AuthGuardService],
+    canActivate: [AuthGuardService],
+    data: {'preload': true},
     loadChildren: 'app/products/product.module#ProductModule'},
   {path: '**', component: PageNotFoundComponent}
 ];
@@ -18,8 +20,10 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     enableTracing: false,
-    useHash: true
+    useHash: true,
+    preloadingStrategy: SelectiveStrategyService
   })],
+  providers: [SelectiveStrategyService],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
