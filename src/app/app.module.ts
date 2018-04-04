6f59462f71a09d6af2a236aed8b36cd1cc895d99
Ed Lee <ronder.lee@gmail.com>
2018-04-04 15:53:45 +0800

    Lazy Loading 1. Preparing for Lazy Loading (要滿足三大要項)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
-import {AppComponent} from './app.component';
-import {WelcomeComponent} from "./home/welcome.component";
-import {PageNotFoundComponent} from "./page-not-found.component";
-import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
-import {ProductData} from "./products/product-data";
-import {ProductModule} from "./products/product.module";
-import {UserModule} from "./user/user.module";
-import {MessageModule} from "./messages/message.module";
-import {HttpClientModule} from "@angular/common/http";
-import {environment} from "../environments/environment";
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ProductData } from './products/product-data';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './messages/message.module';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -26,7 +25,6 @@ import {environment} from "../environments/environment";
     HttpClientModule,
     environment.production ? [] :
     HttpClientInMemoryWebApiModule.forRoot(ProductData, {delay: 2000}),
-    ProductModule,
     UserModule,
     MessageModule,
     AppRoutingModule
